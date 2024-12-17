bbaa0366ffdea81db521ea1c4d9ba224548ba440
claudiajerome07 <claudiajerome07@gmail.com>
2024-12-17 15:00:40 +0530

    created backend for login and signup

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 // const  model = require('mongoose');
-const {CreateUser , verifyUserController} = require('../controller/user.controller.js')
+const {CreateUser , verifyUserController,signup,login} = require('../controller/user.controller.js')
 const upload = require('../middlewares/multer.js')
 // const jwt=require('jasonwebtoken')
 const router = express.Router();
 
 router.post('/create-user', upload.single('file'), CreateUser)
 router.get('/activation/:token',)
+
+router.post("/signup",signup)
+router.post("/login",login)
+
 module.exports = router;
