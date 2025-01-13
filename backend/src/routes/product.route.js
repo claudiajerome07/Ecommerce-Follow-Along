23797871952c0add7425229f9308c8aeb46f2840
claudiajerome07 <claudiajerome07@gmail.com>
2025-01-13 13:46:17 +0530

    milestone14 and 13

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const multer = require('multer')
 const upload = multer({ dest: 'temp-uploads/' })
 const router = express.Router()
-const { createProductController, getProductDataControllerupdateProductController, getSingleProductDocumentController, deleteSingleProductController } = require('../controller/Product.Controller.js')
+const { createProductController, getProductDataController,updateProductController, getSingleProductDocumentController, deleteSingleProductController } = require('../controller/Product.Controller.js')
 
 router.post('/create-product', upload.array('files', 5), createProductController)
 
